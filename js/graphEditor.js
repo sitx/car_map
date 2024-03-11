acc3a71be04663061cab64bcb7f323eb39bb0b9c
Dmitrii Bachin <ska18@mail.ru>
2024-03-11 23:26:53 +0300

    Add selection feature

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -2,6 +2,7 @@ class GraphEditor {
   constructor(canvas, graph) {
     this.canvas = canvas;
     this.graph = graph;
+    this.selected = null;
 
     this.ctx = this.canvas.getContext("2d");
     this.#addEventListeners();
@@ -11,10 +12,14 @@ class GraphEditor {
     this.canvas.addEventListener("mousedown", (evt) => {
       const mouse = new Point(evt.offsetX, evt.offsetY);
       this.graph.addPoint(mouse);
+      this.selected = mouse;
     });
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if (this.selected) {
+      this.selected.draw(this.ctx, { outline: true });
+    }
   }
 }
