7886c60e542058d8fe519f67076055477e965a48
Dmitrii Bachin <ska18@mail.ru>
2024-03-13 18:31:16 +0300

    add selection

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -2,15 +2,23 @@ class GraphEditor {
   constructor(canvas, graph) {
     this.canvas = canvas;
     this.graph = graph;
-    this.selected = null;
 
     this.ctx = this.canvas.getContext("2d");
+
+    this.selected = null;
+    this.hovered = null;
+
     this.#addEventListeners();
   }
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (evt) => {
       const mouse = new Point(evt.offsetX, evt.offsetY);
+      this.hovered = getNearestPoint(mouse, this.graph.points);
+      if (this.hovered) {
+        this.selected = this.hovered;
+        return;
+      }
       this.graph.addPoint(mouse);
       this.selected = mouse;
     });
