fc61f247b62a9a0545973877c3108f6f27c351fc
Dmitrii Bachin <ska18@mail.ru>
2024-03-14 22:18:59 +0300

    viewport and zoom

---
+++ js/viewport.js
@@ -0,0 +1,24 @@
+class Viewport {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.zoom = 1;
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener(
+      "mousewheel",
+      this.#handleMouseWheel.bind(this)
+    );
+  }
+
+  #handleMouseWheel(evt) {
+    const dir = Math.sign(evt.deltaY);
+    const step = 0.1;
+    this.zoom += dir * step;
+    this.zoom = Math.max(1, Math.min(5, this.zoom));
+    console.log(this.zoom);
+  }
+}
